5446262ded30861088613277e1a01cffaffb7bbc
exo <>
2020-09-16 14:34:35 +0200

    fixes

--- example_slug/checker/src/example_slug.py
+++ example_slug/checker/src/example_slug.py
@@ -9,57 +9,12 @@ from ctf_gameserver import checkerlib
 class ExampleChecker(checkerlib.BaseChecker):
 
     def place_flag(self, tick):
-        conn = connect(self.ip)
-        flag = checkerlib.get_flag(tick)
-        conn.sendall('SET {} {}\n'.format(tick, flag).encode('utf-8'))
-        logging.info('Sent SET command: Flag %s', flag)
-
-        try:
-            resp = recv_line(conn)
-            logging.info('Received response to SET command: %s', repr(resp))
-        except UnicodeDecodeError:
-            logging.warning('Received non-UTF-8 data: %s', repr(resp))
-            return checkerlib.CheckResult.FAULTY
-        if resp != 'OK':
-            logging.warning('Received wrong response to SET command')
-            return checkerlib.CheckResult.FAULTY
-
-        conn.close()
         return checkerlib.CheckResult.OK
 
     def check_service(self):
-        conn = connect(self.ip)
-        conn.sendall(b'XXX\n')
-        logging.info('Sent dummy command')
-
-        try:
-            recv_line(conn)
-            logging.info('Received response to dummy command')
-        except UnicodeDecodeError:
-            logging.warning('Received non-UTF-8 data')
-            return checkerlib.CheckResult.FAULTY
-
-        conn.close()
         return checkerlib.CheckResult.OK
 
     def check_flag(self, tick):
-        flag = checkerlib.get_flag(tick)
-
-        conn = connect(self.ip)
-        conn.sendall('GET {}\n'.format(tick).encode('utf-8'))
-        logging.info('Sent GET command')
-
-        try:
-            resp = recv_line(conn)
-            logging.info('Received response to GET command: %s', repr(resp))
-        except UnicodeDecodeError:
-            logging.warning('Received non-UTF-8 data: %s', repr(resp))
-            return checkerlib.CheckResult.FAULTY
-        if resp != flag:
-            logging.warning('Received wrong response to GET command')
-            return checkerlib.CheckResult.FLAG_NOT_FOUND
-
-        conn.close()
         return checkerlib.CheckResult.OK
 
 
